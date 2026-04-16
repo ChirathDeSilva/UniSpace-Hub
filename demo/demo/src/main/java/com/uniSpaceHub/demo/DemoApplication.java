c1db142c20ffbcb6f62fcec9a398615f5888e35a
gayancp <128348582+gayancp@users.noreply.github.com>
2026-04-17 00:16:22 +0530

    Revert "environment setup"
    This reverts commit d8c37d7b771d60ae92a3b08f9e3ee3d9f42132dc.

--- demo/demo/src/main/java/com/uniSpaceHub/demo/DemoApplication.java
+++ demo/demo/src/main/java/com/uniSpaceHub/demo/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UniSpaceHubApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UniSpaceHubApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
