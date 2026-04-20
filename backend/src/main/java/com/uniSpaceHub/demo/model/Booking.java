ead9b7d900b63611eb049a960259f82744830496
Chirath_de_silva <chiraththeekshana2002@gmail.com>
2026-04-20 22:27:04 +0530

    Update Booking.java

--- backend/src/main/java/com/uniSpaceHub/demo/model/Booking.java
+++ backend/src/main/java/com/uniSpaceHub/demo/model/Booking.java
@@ -7,6 +7,9 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 import java.time.LocalTime;
+
+import com.uniSpaceHub.demo.model.FacilitiesModels.Facility;
+
 import java.time.LocalDateTime;
 
 @Entity
